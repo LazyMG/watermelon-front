967055ccea2298ce3408b6d040be2103d87503d9
LazyMG <cbfmark@gmail.com>
2024-05-30 23:46:48 +0900

    css layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,18 +18,43 @@ const NavWrapper = styled.div`
   z-index: 10;
   color: yellow;
   font-weight: 800;
+  display: flex;
+`;
+
+const NavMenu = styled.div`
+  background-color: blue;
+  width: ${({ $open }) => ($open ? "230px" : "100px")};
+`;
+
+const NavContentContainer = styled.div`
+  background-color: red;
+  width: calc(100% - ${({ $open }) => ($open ? "230px" : "100px")});
+  padding-left: 100px;
+  display: flex;
+  padding-right: 100px;
+  justify-content: space-between;
+`;
+
+const NavSearchContainer = styled.div`
+  width: 100px;
+  background-color: green;
+`;
+
+const NavProfileContainer = styled.div`
+  width: 80px;
+  background-color: purple;
 `;
 
 const ContentWrapper = styled.div`
   flex: 1;
-  background-color: red;
+  //background-color: red;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0 100px;
   padding-top: 40px;
-  width: calc(100% - ${({ $open }) => ($open ? "230px" : "0")});
-  margin-left: ${({ $open }) => ($open ? "230px" : "0")};
+  width: calc(100% - ${({ $open }) => ($open ? "230px" : "100px")});
+  margin-left: ${({ $open }) => ($open ? "230px" : "100px")};
   overflow: hidden;
   //height: calc(100vh - 70px);
   //box-sizing: border-box;
@@ -42,12 +67,12 @@ const MenuWrapper = styled.div`
   width: 230px;
   top: 0;
   left: 0;
-  display: ${({ $open }) => ($open ? "block" : "none")};
+  width: ${({ $open }) => ($open ? "230px" : "100px")};
 `;
 
 const Temp = styled.div`
   margin-top: 70px;
-  background-color: blue;
+  //background-color: blue;
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -58,10 +83,19 @@ const Temp = styled.div`
 const Component = styled.div`
   width: 100%;
   height: 600px;
-  background-color: white;
+  background-color: blue;
 `;
 
-const Player = styled.div``;
+const Player = styled.div`
+  position: fixed;
+  background-color: yellow;
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  z-index: 10;
+
+  display: none;
+`;
 
 const Layout = () => {
   const [open, setOpen] = useState(true);
@@ -69,7 +103,13 @@ const Layout = () => {
   const handleClick = () => setOpen(!open);
   return (
     <Wrapper>
-      <NavWrapper>Logo</NavWrapper>
+      <NavWrapper>
+        <NavMenu $open={open} />
+        <NavContentContainer $open={open}>
+          <NavSearchContainer />
+          <NavProfileContainer />
+        </NavContentContainer>
+      </NavWrapper>
       <MenuWrapper $open={open}></MenuWrapper>
       <ContentWrapper $open={open}>
         {/* <Outlet /> */}
